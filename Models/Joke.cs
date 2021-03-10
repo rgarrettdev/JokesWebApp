f898ea105ea02fde5dffa62ebc7fc11619aec95f
Ryan Garrett <ryangarrettdev@gmail.com>
2021-03-10 15:26:06 +0000

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public String JokeQuestion { get; set; }
+        public String JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
